ce7fc9e2d373f28b9f473de2f143a9a8c57d87bf
thewhistledev <firewallvmt@gmail.com>
2023-03-06 00:52:40 +0000

    Added Lib.js library core

---
+++ lib.js
@@ -0,0 +1,123 @@
+const fs = require('fs');
+const util = require('util');
+const EventEmitter = require('events');
+
+
+
+const levels = {
+  DEBUG: 0,
+  INFO: 1,
+  WARN: 2,
+  ERROR: 3,
+  FATAL: 4
+};
+
+class Logger extends EventEmitter {
+  constructor(options = {}) {
+    super();
+
+    this.level = options.level || 'INFO';
+    this.crashReporter = options.crashReporter || null;
+    this.bugReport = options.bugReport || null;
+    this.saveToFile = options.saveToFile || false;
+    this.fileName = options.fileName || 'log.txt';
+
+    if (this.saveToFile) {
+      this.fileStream = fs.createWriteStream(this.fileName, { flags: 'a' });
+      this.fileStream.write('\n\n\n');
+    }
+  }
+
+  debug(message) {
+    if (levels[this.level] <= levels['DEBUG']) {
+      this.log('DEBUG', message);
+    }
+  }
+
+  info(message) {
+    if (levels[this.level] <= levels['INFO']) {
+      this.log('INFO', message);
+    }
+  }
+
+  warn(message) {
+    if (levels[this.level] <= levels['WARN']) {
+      this.log('WARN', message);
+    }
+  }
+
+  error(message, error) {
+    if (levels[this.level] <= levels['ERROR']) {
+      this.log('ERROR', message, error);
+
+      if (this.crashReporter) {
+        this.crashReporter.report(error);
+      }
+    }
+  }
+
+  fatal(message, error) {
+    if (levels[this.level] <= levels['FATAL']) {
+      this.log('FATAL', message, error);
+
+      if (this.crashReporter) {
+        this.crashReporter.report(error);
+      }
+
+      if (this.bugReport) {
+        this.bugReport.report(message, error);
+      }
+
+      process.exit(1);
+    }
+  }
+
+  log(level, message, error = null) {
+    const timestamp = new Date().toISOString();
+
+    const logString = `${timestamp} [${level}] ${message}`;
+
+    if (error) {
+      const stackTrace = util.inspect(error.stack);
+      this.emit('log', `${logString}\n${stackTrace}`);
+    } else {
+      this.emit('log', logString);
+    }
+
+    if (this.saveToFile) {
+      this.fileStream.write(`${logString}\n`);
+
+      if (error) {
+        this.fileStream.write(`${stackTrace}\n`);
+      }
+    }
+  }
+
+  close() {
+    if (this.saveToFile) {
+      this.fileStream.end();
+    }
+  }
+}
+/**
+ * Checks if a string is empty or not.
+ * @param {string} str - The string to check.
+ * @returns {boolean}
+ * @example
+ * isEmpty(''); // true
+ * isEmpty(' '); // false
+ * isEmpty('foo'); // false
+ * isEmpty(' foo'); // false
+ */
+
+class IsEmpty extends String { //extends the string class to add IsEmpty which is not a native method of the string class.
+    constructor() {
+        this.length = 0;
+    }
+    isEmpty() {
+        return this.length === 0;
+    }
+}
+
+
+module.exports = Logger, IsEmpty;
